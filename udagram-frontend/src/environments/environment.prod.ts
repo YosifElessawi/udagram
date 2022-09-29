247fa96ce7437dabb55b730a726d5d01732a83ba
YosifElessawi <yosifelessawi@yahoo.com>
2022-09-30 01:03:25 +0200

    url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-cahm3qh7.us-east-1.elasticbeanstalk.com/",
+  apiHost: "http://udagram-api-dev.eba-cahm3qh7.us-east-1.elasticbeanstalk.com:3000",
 };
 
 /*
