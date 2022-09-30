979d0e676203cc90efce3620dd6fc41af336573b
YosifElessawi <yosifelessawi@yahoo.com>
2022-09-30 02:20:13 +0200

    connection

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-cahm3qh7.us-east-1.elasticbeanstalk.com:3000",
+  apiHost: "http://udagram-api-dev.eba-cahm3qh7.us-east-1.elasticbeanstalk.com/api/v0/",
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-cahm3qh7.us-east-1.elasticbeanstalk.com:3000'
+  apiHost: 'http://udagram-api-dev.eba-cahm3qh7.us-east-1.elasticbeanstalk.com/api/v0/'
 };
 
 /*
